b5d5ac18a260c5af952775d2eaa27a49379538cc
qiu <1258977191@qq.com>
2019-05-24 00:35:51 +0800

    idea tijiao1

--- biyao/src/main/java/com/cmhzteam/util/Test.java
+++ biyao/src/main/java/com/cmhzteam/util/Test.java
@@ -3,8 +3,7 @@ package com.cmhzteam.util;
 public class Test {
     public static void main(String[] args) {
         System.out.println("123");
-
-
-System.out.println("123");
+        System.out.println("123");
+        System.out.println("321");
     }
 }
